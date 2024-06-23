603729528b53e56b5316cc24354f00502f69e1ed
TFKOrig <bhatkhans7@gmail.com>
2024-06-23 15:41:04 +0500

    Added integration of vercel

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import the cors middleware
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import the cors middleware
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "/",
+    methods: ["POST", "GET", "UPDATE", "DELETE"],
+    credentials: true,
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://talha:8pZBLrnrJjJt2emT@cluster0.gmsyp0l.mongodb.net/Audiophile?retryWrites=true&w=majority');
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
+mongoose.connect(
+  "mongodb+srv://talha:8pZBLrnrJjJt2emT@cluster0.gmsyp0l.mongodb.net/Audiophile?retryWrites=true&w=majority"
+);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
 // Body parser middleware
@@ -17,7 +28,7 @@ app.use(bodyParser.json());
 app.use(cors()); // Use the cors middleware
 
 // API routes
-app.use('/api/billing', require('./routes/billings'));
+app.use("/api/billing", require("./routes/billings"));
 
 // Start server
 app.listen(PORT, () => {
